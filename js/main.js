49a3fc3b5be46de10a388cab15203a93c28480f4
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-09 13:36:16 +0100

    ajax-call

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		
+		albums: []
+	},
+	mounted(){
+
+		axios
+		.get('https://flynn.boolean.careers/exercises/api/array/music')
+		.then(result => {
+			console.log(result.data.response);
+			this.albums = result.data.response;
+			console.log(this.albums);
+		});
+
 	}
 }); 
