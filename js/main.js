289a8e727753e50cbc737040a1e0e356667c3ff6
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-09 13:53:05 +0100

    filter-for-genre

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		albums: []
+		albums: [],
+		types: []
+	},
+	methods: {
+
+		// Funzione per filtrare tutti i vari generi musicali
+		filterType(){
+
+			this.albums.forEach(element => {
+				if (!this.types.includes(element.genre)){
+					this.types.push(element.genre);
+				}
+			});
+
+		}
+
 	},
 	mounted(){
 
+		//Chiamata ajax
 		axios
 		.get('https://flynn.boolean.careers/exercises/api/array/music')
 		.then(result => {
-			console.log(result.data.response);
+
+			// Il mio array sarà uguale all'array di oggetti che dovrò utilizzare
 			this.albums = result.data.response;
-			console.log(this.albums);
+
+			//Richiamo la funzione
+			this.filterType();
+
 		});
 
 	}
